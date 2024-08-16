f0a9964e75e31adedc1910ec16b13129749afd60
tiago002pro <tiagobarbosa02@outlook.com>
2024-08-15 21:06:12 -0300

    Instalando o native base e criando a Theme

---
+++ src/styles/Theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from "native-base";
+
+export const THEME = extendTheme({
+  colors: {
+    primary: '#8A2BE2',
+    secondary: '#f1c40f',
+    background: '#0D0D0D',
+    tabbar: '#030303',
+    headers: '#030303',
+  }
+});
