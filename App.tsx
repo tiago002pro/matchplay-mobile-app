87a351518652e7e5c9a2ea5ed12eb4c4c17b5062
tiago002pro <tiagobarbosa02@outlook.com>
2024-09-08 13:31:49 -0300

    Ignorando logs

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes';
 import { NativeBaseProvider, StatusBar } from 'native-base';
-import 'react-native-gesture-handler';
 import { AuthProvider } from './src/context/AuthContext';
+import { LogBox } from 'react-native';
 
 export default function App() {
+  useEffect(() => {
+    LogBox.ignoreLogs([
+      'In React 18, SSRProvider is not necessary and is a noop. You can remove it from your app.',
+    ]);
+  }, []);
+
   return (
     <NativeBaseProvider>
       <NavigationContainer>
