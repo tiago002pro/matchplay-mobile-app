88577b5de85ff9cd96259719c923fdfeebfcf4a8
tiago002pro <tiagobarbosa02@outlook.com>
2024-10-28 23:08:54 -0300

    Criação do input component

---
+++ src/components/InputComponent.tsx
@@ -0,0 +1,45 @@
+import { CloseIcon, Input, SearchIcon } from "native-base";
+import { Pressable } from "react-native";
+import { THEME } from "../styles/Theme";
+
+interface MyInputProps {
+  key?:any;
+  onChangeText?:any;
+  placeholder?:string;
+  value?:any;
+  rightElementFunction?:any;
+}
+
+export function InputComponent({ key, onChangeText, placeholder, value, rightElementFunction }:MyInputProps) {
+  return(
+    <Input
+      key={key}
+      onChangeText={onChangeText}
+      placeholder={placeholder}
+      value={value}
+      style={{
+        fontSize: THEME.fontSizes.md,
+      }}
+      leftElement={ <SearchIcon size="6" ml={2} color={THEME.colors.font} /> }
+      rightElement={
+        <Pressable onPress={rightElementFunction}>
+          <CloseIcon size="5" mr={2} color={THEME.colors.font} />
+        </Pressable>
+      }
+      color={THEME.colors.font}
+      borderColor={THEME.colors.primary}
+      bg={THEME.colors.background}
+      selectionColor={THEME.colors.white}
+      _focus={{
+        backgroundColor: THEME.colors.background,
+        borderColor: THEME.colors.primary,
+        _ios: {
+          selectionColor: THEME.colors.primary,
+        },
+        _android: {
+          selectionColor: THEME.colors.primary,
+        }
+      }}
+    />
+  );
+}
