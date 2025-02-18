cfac90a84f9b9889ed08cdb64ede3fbb501a7a01
tiago002pro <tiagobarbosa02@outlook.com>
2025-02-18 10:23:20 -0300

    Inserindo os icones de jogos

---
+++ src/shared/platformIcons.ts
@@ -0,0 +1,67 @@
+import PC from "../../assets/icons/pc.svg";
+import PlayStation from "../../assets/icons/playstation.svg";
+import Xbox from "../../assets/icons/xbox.svg";
+import iOS from "../../assets/icons/ios.svg";
+import Android from "../../assets/icons/android.svg";
+import Apple from "../../assets/icons/apple.svg";
+import Linux from "../../assets/icons/linux.svg";
+import Nitendo from "../../assets/icons/nitendo.svg";
+import Atari from "../../assets/icons/atari.svg";
+import Commodore from "../../assets/icons/commodore.svg";
+import SEGA from "../../assets/icons/sega.svg";
+import _3DO from "../../assets/icons/sega.svg";
+import Web from "../../assets/icons/web.svg";
+
+export const platformIcons = {
+  "PC": PC,
+  "PlayStation 5": PlayStation,
+  "PlayStation 4": PlayStation,
+  "PlayStation 3": PlayStation,
+  "PlayStation 2": PlayStation,
+  "PlayStation": PlayStation,
+  "PS Vita": PlayStation,
+  "PSP": PlayStation,
+  "Xbox One": Xbox,
+  "Xbox Series S/X": Xbox,
+  "Xbox 360": Xbox,
+  "Xbox": Xbox,
+  "iOS": iOS,
+  "Android": Android,
+  "macOS": Apple,
+  "Classic Macintosh": Apple,
+  "Apple II": Apple,
+  "Linux": Linux,
+  "Nintendo Switch": Nitendo,
+  "Nintendo 3DS": Nitendo,
+  "Nintendo DS": Nitendo,
+  "Nintendo DSi": Nitendo,
+  "Wii U": Nitendo,
+  "Wii": Nitendo,
+  "GameCube": Nitendo,
+  "Nintendo 64": Nitendo,
+  "Game Boy Advance": Nitendo,
+  "Game Boy Color": Nitendo,
+  "Game Boy": Nitendo,
+  "SNES": Nitendo,
+  "NES": Nitendo,
+  "Atari 7800": Atari,
+  "Atari 5200": Atari,
+  "Atari 2600": Atari,
+  "Atari Flashback": Atari,
+  "Atari 8-bit": Atari,
+  "Atari ST": Atari,
+  "Atari Lynx": Atari,
+  "Atari XEGS": Atari,
+  "Jaguar": Atari,
+  "Commodore / Amiga": Commodore,
+  "Genesis": SEGA,
+  "SEGA Saturn": SEGA,
+  "SEGA CD": SEGA,
+  "SEGA 32X": SEGA,
+  "SEGA Master System": SEGA,
+  "Dreamcast": SEGA,
+  "Game Gear": SEGA,  
+  "3DO": _3DO,
+  "Neo Geo": Nitendo,
+  "web": Web,
+};
