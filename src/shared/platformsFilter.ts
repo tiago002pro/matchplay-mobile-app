311b1b107c5ae8beb3c61e4d94c22ad3ea981a62
tiago002pro <tiagobarbosa02@outlook.com>
2025-02-24 22:48:41 -0300

    Filtro de acordo com os dados de plataforma do rawggame

---
+++ src/shared/platformsFilter.ts
@@ -0,0 +1,15 @@
+export const PlatformsFilter:{name:string, ids:number[]}[] = [
+  { name: `PC`, ids: [4] },
+  { name: `PlayStation`, ids: [187, 18, 16, 15, 27, 19, 17] },
+  { name: `Xbox`, ids: [1, 14, 80] },
+  { name: `iOS`, ids: [3] },
+  { name: `Android`, ids: [21] },
+  { name: `Apple`, ids: [5, 55, 41] },
+  { name: `Linux`, ids: [6] },
+  { name: `Nintendo`, ids: [7, 8, 9, 13, 10, 11, 105, 83, 24, 43, 26, 79, 49] },
+  { name: `Atari`, ids: [28, 31, 23, 22, 25, 34, 46, 50, 112] },
+  { name: `Commodore`, ids: [166] },
+  { name: `SEGA`, ids: [167, 107, 119, 117, 74, 106, 77] },
+  { name: `3DO`, ids: [111] },
+  { name: `Neo Geo`, ids: [12] },
+]
