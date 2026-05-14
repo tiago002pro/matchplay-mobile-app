8655829c679c6375aac355d9acfd98143e363e81
tiago002pro <tiagobarbosa02@outlook.com>
2026-05-14 00:16:24 -0300

    Correções para o build

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,15 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = (() => {
-  const config = getDefaultConfig(__dirname);
-  const { transformer, resolver } = config;
+const config = getDefaultConfig(__dirname);
 
-  config.transformer = {
-    ...transformer,
-    babelTransformerPath: require.resolve("react-native-svg-transformer"),
-  };
-  config.resolver = {
-    ...resolver,
-    assetExts: resolver.assetExts.filter(ext => ext !== "svg"),
-    sourceExts: [...resolver.sourceExts, "svg"],
-  };
+config.transformer.babelTransformerPath = require.resolve(
+  "react-native-svg-transformer"
+);
 
-  return config;
-})();
+config.resolver.assetExts = config.resolver.assetExts.filter(
+  (ext) => ext !== "svg"
+);
+
+config.resolver.sourceExts.push("svg");
+
+module.exports = config;
